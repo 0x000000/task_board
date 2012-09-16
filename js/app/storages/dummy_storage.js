996ca271f044c714e50477eec114f62198d4637f
Alexander Rudenko <mur.mailbox@gmail.com>
2012-09-16 19:56:49 +0300

    Fix status name for dummy data

--- js/app/storages/dummy_storage.js
+++ js/app/storages/dummy_storage.js
@@ -11,31 +11,31 @@
         id:    10,
         name:  "Et reiciendis doloremque eius molestiae odit laudantium qui molestias",
         type:  'task',
-        state: 'todo'
+        status: 'todo'
       },
       {
         id:    12,
         name:  "Eum et et animi omnis consectetur dolorem architecto facilis",
         type:  'task',
-        state: 'todo'
+        status: 'todo'
       },
       {
         id:    13,
         name:  "Sunt animi modi fuga eaque iusto",
         type:  'bug',
-        state: 'in_progress'
+        status: 'in_progress'
       },
       {
         id:    20,
         name:  "Ab quos cumque non similique",
         type:  'task',
-        state: 'done'
+        status: 'done'
       },
       {
         id:    21,
         name:  "Vel optio doloremque officia ducimus",
         type:  'bug',
-        state: 'done'
+        status: 'done'
       }
     ]);
   };
